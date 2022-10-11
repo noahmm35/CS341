bb5a152a26c85cc2616848d0c45c7280a4c8e1ee
Monroe, Noah <nmmonroe@butler.edu>
2022-10-11 13:41:41 -0400

    Update driver3.cpp

--- driver3.cpp
+++ driver3.cpp
@@ -1,42 +1,35 @@
 #include "bitarray.h"
+#include "set.h"
 #include <iostream>
 #include <cstring>
 
 int main()
 {	
 	char test[] = "Noah";
+	char test2[] = "Taco";
 	
-	BitArray Name(strlen(test));
+	Set Name(strlen(test));
+	Set Food(strlen(test2));
 	
 	Name.initialize(test,strlen(test));
+	Food.initialize(test2,strlen(test2));
 	
-	Name.print();
+	Name.getData();
+	Food.getData();
 	std::cout << std::endl;
 	
-	std::cout << "0th bit set: " << Name.get(0) << std::endl;
-	std::cout << "10th bit set: " << Name.get(10) << std::endl << std::endl;
-	
-	std::cout << "Setting 0th bit to 1..." << std::endl;
-	Name.flip(0);
-	Name.print();
-	
-	std::cout << std::endl;
-	
-	std::cout << "Setting 10th bit to 0..." << std::endl;
-	Name.flip(10);
-	Name.print();
+	std::cout << "Cardinality (name): " << Name.getCardinality() << std::endl;
+	std::cout << "Cardinality (food): " << Food.getCardinality() << std::endl;
 	
 	std::cout << std::endl;
 	
-	std::cout << "Complement: " << std::endl;
-	Name.complement();
-	Name.print(); 
-	
+	std::cout << "Set Union A U B: " << std::endl;
+	Name.setUnion(Food);
 	std::cout << std::endl;
 	
-	std::cout << "Clear BitArray: " << std::endl;
-	Name.clear();
-	Name.print(); 
+	std::cout << "Set Intersection A x B: " << std::endl;
+	Name.setIntersection(Food);
+	std::cout << std::endl;
 	
 	return 0;
 }
